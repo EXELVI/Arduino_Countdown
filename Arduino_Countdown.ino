d1d03cd99b0efa0d82eb4bd733485e9b690dbfbc
EXELVI <exelvi@outlook.com>
2024-07-27 13:27:51 +0200

    chore: Added a OLED Display, made the code work anyway if there is no wifi

--- Arduino_Countdown.ino
+++ Arduino_Countdown.ino
@@ -4,16 +4,25 @@
 #include "WiFiSSLClient.h"
 #include <SPI.h>
 #include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include "RTC.h"
 
 #include "arduino_secrets.h"
+#include "icons.h"
 
 const char *ssid = SECRET_SSID;
 const char *pass = SECRET_PASS;
 
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
 int status = WL_IDLE_STATUS;
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP);
+RTCTime currentTime;
+
+bool updatedWithNTP = false;
 
 void setup()
 {
@@ -23,18 +32,29 @@ void setup()
     RTCTime savedTime;
     RTC.getTime(savedTime);
 
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    display.print("Countdown to");
+    display.setCursor(0, 10);
+    display.print("2024-07-27 23:59:59"); //This date is an example 
+    display.display();
+    delay(2000);
+
     Serial.print("Saved time: ");
-    Serial.print(savedTime.getYear(););
+    Serial.print(savedTime.getYear());
     Serial.print("-");
-    Serial.print(Month2int(savedTime.getMonth()););
+    Serial.print(Month2int(savedTime.getMonth()));
     Serial.print("-");
-    Serial.print(savedTime.getDayOfMonth(););
+    Serial.print(savedTime.getDayOfMonth());
     Serial.print(" ");
-    Serial.print(savedTime.getHour(););
+    Serial.print(savedTime.getHour());
     Serial.print(":");
-    Serial.print(savedTime.getMinutes(););
+    Serial.print(savedTime.getMinutes());
     Serial.print(":");
-    Serial.println(savedTime.getSeconds(););
+    Serial.println(savedTime.getSeconds());
 
     if (!RTC.isRunning())
     {
@@ -49,54 +69,58 @@ void setup()
             RTC.setTime(savedTime);
         }
     }
-
-    if (WiFi.status() == WL_NO_MODULE)
-    {
-        Serial.println("Communication with WiFi module failed!");
-        while (true)
-            ;
-    }
-
-    String fv = WiFi.firmwareVersion();
-    if (fv < WIFI_FIRMWARE_LATEST_VERSION)
-    {
-        Serial.println("Please upgrade the firmware");
-    }
-
-    while (status != WL_CONNECTED)
-    {
-        Serial.print("Attempting to connect to SSID: ");
-        Serial.println(ssid);
-        status = WiFi.begin(ssid, pass);
-    }
-
-    Serial.println("Connected to WiFi network");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-
-    timeClient.begin();
-
-    timeClient.update();
-
-    auto unixTime = timeClient.getEpochTime();
-    Serial.print("Unix time = ");
-    Serial.println(unixTime);
-    if (unixTime > 0)
-    {
-        RTCTime timeToSet = RTCTime(unixTime);
-        RTC.setTime(timeToSet);
-
-        RTC.getTime(currentTime);
-        Serial.println("The RTC was just set to: " + String(currentTime));
-    }
-    else
-    {
-        Serial.println("Failed to get time from NTP server, will use the last saved time");
-    }
 }
 
 void loop()
 {
+    if (!updatedWithNTP)
+    {
+        if (WiFi.status() == WL_NO_MODULE)
+        {
+            Serial.println("Communication with WiFi module failed!");
+            updatedWithNTP = true; // we will use the last saved time
+        }
+
+        String fv = WiFi.firmwareVersion();
+        if (fv < WIFI_FIRMWARE_LATEST_VERSION)
+        {
+            Serial.println("Please upgrade the firmware");
+        }
+
+        if (status != WL_CONNECTED)
+        {
+            Serial.print("Attempting to connect to SSID: ");
+            Serial.println(ssid);
+            status = WiFi.begin(ssid, pass);
+        }
+        else
+        {
+            Serial.println("Connected to WiFi network");
+            Serial.println("IP address: ");
+            Serial.println(WiFi.localIP());
+
+            timeClient.begin();
+
+            timeClient.update();
+
+            auto unixTime = timeClient.getEpochTime();
+            Serial.print("Unix time = ");
+            Serial.println(unixTime);
+            if (unixTime > 0)
+            {
+                RTCTime timeToSet = RTCTime(unixTime);
+                RTC.setTime(timeToSet);
+
+                RTC.getTime(currentTime);
+                Serial.println("The RTC was just set to: " + String(currentTime));
+            }
+            else
+            {
+                Serial.println("Failed to get time from NTP server, will use the last saved time");
+            }
+            updatedWithNTP = true;
+        }
+    }
 
     RTCTime now;
     RTC.getTime(now);
